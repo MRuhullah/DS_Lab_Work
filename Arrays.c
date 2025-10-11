b984b80821c43a358e9f1bb62cdf27f82972c4dc
MRuhullah <ateebqazim11@gmail.com>
2025-10-11 12:27:15 +0530

    Add: Insertion

--- Arrays.c
+++ Arrays.c
@@ -11,7 +11,6 @@ void inputArray(int arr[], int n)
 
 void displayArray(int arr[], int n)
 {
-    printf("Array elements are:\n");
     for (int i = 0; i < n; i++)
     {
         printf("%d ", arr[i]);
@@ -19,16 +18,83 @@ void displayArray(int arr[], int n)
     printf("\n");
 }
 
+int insertion_beg(int arr[], int n)
+{
+    int element;
+    printf("Enter the element you wanna insert at the beginning: ");
+    scanf("%d", &element);
+
+    for (int i = n; i > 0; i--)
+    {
+        arr[i] = arr[i - 1];
+    }
+
+    arr[0] = element;
+    return n + 1;
+}
+
+int insertion_end(int arr[], int n)
+{
+    int element;
+    printf("Enter the element you wanna insert at the end: ");
+    scanf("%d", &element);
+
+    arr[n] = element;
+    return n + 1;
+}
+
+int insertion_any(int arr[],int n)
+{
+    int element,position;
+    printf("Enter at which position you wanna insert from 0 to %d: ",n);
+    scanf("%d",&position);
+    if(position < 0 || position > n)
+    {
+    printf("Invalid position!\n");
+    return n;
+    }
+
+    printf("Enter the element you wanna insert: ");
+    scanf("%d", &element);
+
+    for(int i = n; i>position;i--)
+    {
+        arr[i] = arr[i-1];
+    }
+
+    arr[position] = element;
+    return n+1;
+
+}
+
 int main()
 {
     int n;
-
-    printf("Enter the number of elements: ");
+    printf("Enter the number of elements (max 99): ");
     scanf("%d", &n);
 
-    int arr[n];
+    if (n > 99)
+    {
+        printf("Too many elements!\n");
+        return 1;
+    }
+
+    int arr[100];
 
     inputArray(arr, n);
+    printf("Array elements are:\n");
+    displayArray(arr, n);
+
+    n = insertion_beg(arr, n);
+    printf("Updated elements are:\n");
+    displayArray(arr, n);
+
+    n = insertion_end(arr,n);
+    printf("Updated elements are:\n");
+    displayArray(arr, n);
+
+    n = insertion_any(arr,n);
+    printf("Updated elements are:\n");
     displayArray(arr, n);
 
     return 0;

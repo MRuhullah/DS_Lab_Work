d37836dc6310236aea18eddd0a04c992095ee2e1
MRuhullah <ateebqazim11@gmail.com>
2025-10-12 11:52:34 +0530

    Add deletions

--- Ops_LL.c
+++ Ops_LL.c
@@ -3,18 +3,18 @@
 struct node
 {
     int data;
-    struct node *next;
+    struct node* next;
 };
-struct node *createnode(int value)
+struct node* createnode(int value)
 {
-    struct node *newnode = (struct node *)malloc(sizeof(struct node));
+    struct node* newnode = (struct node *)malloc(sizeof(struct node));
     newnode->data = value;
     newnode->next = NULL;
     return newnode;
 }
 void printlist(struct node *head)
 {
-    struct node *tail = head;
+    struct node* tail = head;
     printf("Linked List: ");
     while (tail != NULL)
     {
@@ -23,7 +23,115 @@ void printlist(struct node *head)
     }
     printf("NULL\n");
 }
-int main()
+struct node* insertion_beg(struct node *head, int value)
+{
+    struct node* newnode = createnode(value);
+    newnode->next = head;
+    head = newnode;
+    return head;
+}
+struct node* insertion_end(struct node *head, int value)
+{
+    struct node* newnode = createnode(value);
+    if (head == NULL)
+        return newnode;
+
+    struct node* temp = head;
+    while (temp->next != NULL)
+        temp = temp->next;
+       temp->next = newnode;
+    return head;
+}
+struct node* insertion_any(struct node *head, int value, int position)
+{
+    struct node* newnode = createnode(value);
+    if (position == 0)
+    {
+        newnode->next = head;
+        return newnode;
+    }
+    struct node* temp = head;
+    int count = 0;
+    while (temp != 0 && count < position - 1)
+    {
+        temp = temp->next;
+        count++;
+    }
+    if (temp == NULL)
+    {
+        printf("Invalid position!");
+        return head;
+    }
+    newnode->next = temp->next;
+    temp->next = newnode;
+    return head;
+}
+
+struct node *delete_beg(struct node *head)
+{
+
+    if (head == NULL)
+    {
+        printf("List is already empty\n");
+        return NULL;
+    }
+    struct node *temp = head;
+    head = head->next;
+    free(temp);
+    return head;
+}
+struct node* delete_end(struct node* head)
+{
+    if (head == NULL)
+    {
+        printf("List is already empty\n");
+        return NULL;
+    }
+
+    if (head->next == NULL)
+    {
+        free(head);
+        return NULL;
+    }
+
+    struct node *temp = head;
+    struct node *prev;
+
+    while (temp->next != NULL)
+    {
+        prev = temp;
+        temp = temp->next;
+    }
+    prev->next = NULL;
+    free(temp);
+    return head;
+}
+struct node* delete_position(struct node* head, int position)
+{
+     if (head == NULL)
+    {
+        printf("List is already empty.\n");
+        return NULL;
+    }
+     struct node* temp = head;
+     struct node* prev;
+     int count = 0;
+      while (temp != NULL && count < position)
+    {
+        prev = temp;
+        temp = temp->next;
+        count++;
+    }
+    if (temp == NULL)
+    {
+        printf("Invalid position: No node at index %d.\n", position);
+        return head;
+    }
+    prev->next = temp->next;
+    free(temp);
+    return head;
+}
+    int main()
 {
     int n, value;
     struct node *head = NULL, *tail = NULL;
@@ -40,6 +148,39 @@ int main()
             tail->next = newnode;
         tail = newnode;
     }
+
+    printf("Enter the value you wanna insert");
+    scanf("%d", &value);
+    head = insertion_beg(head, value);
+    printf("Updated Linked List: ");
     printlist(head);
+
+    printf("Enter value to insert at end: ");
+    scanf("%d", &value);
+    head = insertion_end(head, value);
+    printf("Updated Linked List: ");
+    printlist(head);
+
+    int position;
+    printf("Enter the position at which you wanna insert node: \n");
+    scanf("%d", &position);
+    printf("Enter value to insert at end: ");
+    scanf("%d", &value);
+    head = insertion_any(head, value, position);
+    printf("Updated Linked List: ");
+    printlist(head);
+
+    head = delete_beg(head);
+    printlist(head);
+
+    head = delete_end(head);
+    printlist(head);
+
+    printf("Enter position to delete (0-based index): ");
+    scanf("%d", &position);
+    head = delete_position(head, position);
+    printlist(head);
+
+
     return 0;
 }

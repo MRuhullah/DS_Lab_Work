170f19743e106296115049881f6d791d40c59177
MRuhullah <ateebqazim11@gmail.com>
2025-12-28 09:12:23 +0530

    Assignment

---
+++ middleEle_LL.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node
+{
+    int data;
+    struct Node *next;
+};
+
+struct Node *newNode(int data)
+{
+    struct Node *node = (struct Node *)malloc(sizeof(struct Node));
+    node->data = data;
+    node->next = NULL;
+    return node;
+}
+
+void findMiddle(struct Node *head)
+{
+    struct Node *slow = head;
+    struct Node *fast = head;
+
+    while (fast != NULL && fast->next != NULL)
+    {
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+
+    printf("The middle element is: %d\n", slow->data);
+}
+
+int main()
+{
+
+    struct Node *head = newNode(1);
+    head->next = newNode(2);
+    head->next->next = newNode(3);
+    head->next->next->next = newNode(4);
+    // head->next->next->next->next = newNode(5);
+
+    findMiddle(head);
+
+    return 0;
+}
